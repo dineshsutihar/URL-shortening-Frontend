bac326ef60f424b1e6024964a6641272b1a4386c
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-10-08 23:48:50 +0530

    add: add tailwindcss and also modify the readme docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
 
   return (
-    <>
-      Hello World
-    </>
+    <div className="text-3xl font-bold underline bg-sky-800 h-screen">
+      <h1 className="text-white">Tailwind CSS setup check</h1>
+    </div>
   )
 }
 
