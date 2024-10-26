7c1719e4509c940d6ec9eec94a953c2b49c96913
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-10-26 23:59:55 +0530

    fix: footer fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
     return (
-        <footer className="text-white bg-[hsl(255,11%,22%)] p-10 flex flex-col md:flex-row items-center md:items-start md:justify-center gap-8 md:gap-20 text-center">
+        <footer className="text-white bg-[hsl(260,8%,14%)] p-10 flex flex-col md:flex-row items-center md:items-start md:justify-center gap-8 text-center">
             {/* Logo Section */}
             <div>
                 <img src="./public/images/logo-white.svg" alt="Logo White" />
             </div>
 
             {/* Link Sections */}
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 text-nowrap">
                 <div className="flex flex-col items-center md:items-start space-y-2">
                     <h2 className="font-semibold">Features</h2>
                     <a href="#" className="text-gray-400 hover:text-white">Link Shortening</a>
@@ -32,7 +32,7 @@ export default function Footer() {
             </div>
 
             {/* Social Media Icons */}
-            <div className="flex space-x-6 mt-8 md:mt-0">
+            <div className="flex space-x-6 md:space-x-3 mt-8 md:mt-0 min-w-40">
                 <a href="#"><img src="./public/images/icon-facebook.svg" alt="Facebook" className="w-6 h-6" /></a>
                 <a href="#"><img src="./public/images/icon-twitter.svg" alt="Twitter" className="w-6 h-6" /></a>
                 <a href="#"><img src="./public/images/icon-pinterest.svg" alt="Pinterest" className="w-6 h-6" /></a>
