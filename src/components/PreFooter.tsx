445af33924f419c8817273931fa5298bdda16930
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-10-26 23:53:04 +0530

    style: allign the color according to the design

--- src/components/PreFooter.tsx
+++ src/components/PreFooter.tsx
@@ -1,17 +1,17 @@
 export default function PreFooter() {
     return (
         <section>
-            <div className="relative h-max bg-gray-900 overflow-hidden">
+            <div className="relative h-max bg-[hsl(257,27%,26%)] overflow-hidden">
                 <picture>
                     <source media="(min-width: 768px)" srcSet="/images/bg-boost-desktop.svg" />
                     <img
-                        className="w-full object-cover absolute inset-0"
+                        className="w-full h-full object-cover absolute inset-0"
                         src="/images/bg-boost-mobile.svg"
                         alt="URL shortener background"
                     />
                 </picture>
                 <div className="relative p-6 flex flex-col justify-center py-20 items-center gap-5">
-                    <h1 className="text-3xl text-white font-bold">Boost your links today</h1>
+                    <h1 className="text-3xl text-white text-center font-bold">Boost your links today</h1>
                     <button className="md:w-1/5 px-5 py-3 text-xl font-semibold text-white bg-[hsl(180,66%,49%)] rounded-3xl hover:bg-opacity-80 transition-colors">
                         Get Started
                     </button>
