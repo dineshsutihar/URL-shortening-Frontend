09c4201ebeefe6e0bdcd32f3260c7a7a75501e71
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-11-23 16:20:45 +0530

    Toster is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Footer from "./components/Footer"
-import Home from "./components/Home"
-import Nav from "./components/Nav"
-import PreFooter from "./components/PreFooter"
-import ShortenLink from "./components/ShortenLink"
-import Statistics from "./components/Statistics"
+import { Toaster } from "sonner";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import PreFooter from "./components/PreFooter";
+import ShortenLink from "./components/ShortenLink";
+import Statistics from "./components/Statistics";
 
 function App() {
-
   return (
     <>
       <div className="max-w-screen-2xl mx-auto">
@@ -16,11 +16,12 @@ function App() {
           <ShortenLink />
           <Statistics />
         </main>
+        <Toaster />
       </div>
       <PreFooter />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ShortenLink.tsx
+++ src/components/ShortenLink.tsx
@@ -1,29 +1,40 @@
+import { toast } from "sonner";
+
 export default function ShortenLink() {
-    return (
-        <div className="relative bg-gray-900 border border-amber-400 rounded-xl mt-32 mx-4 md:mx-0">
-            <picture className="absolute inset-0 h-full rounded-xl">
-                <source media="(min-width: 768px)" srcSet="/images/bg-shorten-desktop.svg" />
-                <img
-                    className="w-full h-full object-cover rounded-xl"
-                    src="/images/bg-shorten-mobile.svg"
-                    alt="URL shortener background"
-                />
-            </picture>
-            <div className="relative z-10 p-6 md:p-10 flex flex-col md:flex-row justify-center items-center gap-4">
-                <input
-                    type="text"
-                    name="longlink"
-                    placeholder="Shorten a link here..."
-                    className="w-full px-4 py-3 rounded focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                />
-                <button
-                    className="w-full md:w-40 px-5 py-3 text-xl font-semibold text-white 
+  return (
+    <div className="relative bg-gray-900 border border-amber-400 rounded-xl mt-32 mx-4 md:mx-0">
+      <picture className="absolute inset-0 h-full rounded-xl">
+        <source
+          media="(min-width: 768px)"
+          srcSet="/images/bg-shorten-desktop.svg"
+        />
+        <img
+          className="w-full h-full object-cover rounded-xl"
+          src="/images/bg-shorten-mobile.svg"
+          alt="URL shortener background"
+        />
+      </picture>
+      <div className="relative z-10 p-6 md:p-10 flex flex-col md:flex-row justify-center items-center gap-4">
+        <input
+          type="text"
+          name="longlink"
+          placeholder="Shorten a link here..."
+          className="w-full px-4 py-3 rounded focus:outline-none focus:ring-2 focus:ring-cyan-500"
+        />
+        <button
+          className="w-full md:w-40 px-5 py-3 text-xl font-semibold text-white 
                              bg-[hsl(180,66%,49%)] rounded whitespace-nowrap 
                              hover:bg-opacity-80 transition-colors duration-300"
-                >
-                    Shorten It!
-                </button>
-            </div>
-        </div>
-    );
+          onClick={() =>
+            toast("Hyy! Please Wait... I'm working on it", {
+              description: "I am working hard to bring this soon!",
+              position: "top-right",
+            })
+          }
+        >
+          Shorten It!
+        </button>
+      </div>
+    </div>
+  );
 }
