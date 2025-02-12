f39635b3e6260033a31d9e70239de5dc39dfdb29
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-02-13 01:08:50 +0530

    URl shorting functionality added

--- src/components/ShortenLink.tsx
+++ src/components/ShortenLink.tsx
@@ -1,6 +1,26 @@
 import { toast } from "sonner";
+import axios from "axios";
+import { useState } from "react";
 
 export default function ShortenLink() {
+  const [url, setUrl] = useState("");
+  const shortenLink = async () => {
+    try {
+      const response = await axios.post("http://localhost:3031/shorten", {
+        url: url,
+      });
+      console.log(response.data);
+      toast.success("Link shortened successfully", {
+        description: response.data,
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Error while shortening the link", {
+        description: "Please try again later",
+      });
+    }
+  };
+
   return (
     <div className="relative bg-gray-900 border border-amber-400 rounded-xl mt-32 mx-4 md:mx-0">
       <picture className="absolute inset-0 h-full rounded-xl">
@@ -20,17 +40,14 @@ export default function ShortenLink() {
           name="longlink"
           placeholder="Shorten a link here..."
           className="w-full px-4 py-3 rounded focus:outline-none focus:ring-2 focus:ring-cyan-500"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
         />
         <button
           className="w-full md:w-40 px-5 py-3 text-xl font-semibold text-white 
                              bg-[hsl(180,66%,49%)] rounded whitespace-nowrap 
                              hover:bg-opacity-80 transition-colors duration-300"
-          onClick={() =>
-            toast("Hyy! Please Wait... I'm working on it", {
-              description: "I am working hard to bring this soon!",
-              position: "top-right",
-            })
-          }
+          onClick={() => shortenLink()}
         >
           Shorten It!
         </button>
